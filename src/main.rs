768b8bad0b034ddb061dfc8117a3948ea6f2f06e
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-12-08 17:24:08 +0300

    initial test code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use lambda_http::{service_fn, Error, RequestExt, IntoResponse, Request};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda_http::run(service_fn(hello)).await?;
+    Ok(())
+}
+
+async fn hello(
+    request: Request
+) -> Result<impl IntoResponse, std::convert::Infallible> {
+    let _context = request.lambda_context_ref();
+
+    Ok(format!(
+        "hello {}",
+        request
+            .query_string_parameters_ref()
+            .and_then(|params| params.first("name"))
+            .unwrap_or_else(|| "stranger")
+    ))
 }
