ec5a69a7ca57a065617a8542ba672e2e5fc88e64
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-12-18 21:01:53 +0300

    initialize the config file with default values if it does not exist.

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,6 +1,7 @@
+use tokio::fs::{read_to_string, write};
 use serde::{Serialize, Deserialize};
 use std::error::Error as StdError;
-use tokio::fs::read_to_string;
+use std::io::ErrorKind;
 use std::env::var;
 use super::*;
 
@@ -18,7 +19,25 @@ pub struct Config {
 impl Config {
     pub async fn read() -> Result<Self> {
         let path = var("CONFIG").unwrap_or(String::from("./config.json"));
-        let json = read_to_string(path).await?;
+        let json = match read_to_string(&path).await {
+            Ok(json) => json,
+            Err(err) => {
+                match err.kind() {
+                    ErrorKind::NotFound => {
+                        let config = Config{mail: Mail::from_env()?, database: Default::default()};
+                        config.write(&path).await?;
+                        return Ok(config)
+                    },
+                    _ => Err(err)?
+                }
+            }
+        };
         Ok(serde_json::from_str(&json)?)
+    }
+
+    async fn write(&self, path: &str) -> Result<()> {
+        let json = serde_json::to_string(self)?;
+        let contents = json.as_bytes();
+        Ok(write(path, contents).await?)
     }
 }
