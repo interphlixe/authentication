03204a64c32bdc63508951aa892bfcb2bd89bf4b
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-12-19 09:38:55 +0300

    use a singlepart body of content-type: Html

--- src/domain/services/mail.rs
+++ src/domain/services/mail.rs
@@ -1,14 +1,15 @@
-use lettre::{Message, message::Mailbox, AsyncTransport};
+use lettre::{Message, message::{Mailbox, SinglePart}, AsyncTransport};
 use std::env::var;
 use crate::Mailer;
 
 
 pub async fn send_mail(mailer: Mailer, sender: Mailbox, receiver: Mailbox, subject: &str, message: String) -> Result<(), Box<dyn std::error::Error>> {
+    let body = SinglePart::html(message);
     let email = Message::builder()
     .from(sender)
     .to(receiver)
     .subject(subject)
-    .body(message)?;
+    .singlepart(body)?;
 
     mailer.send(email).await?;
     Ok(())
