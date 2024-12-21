cad9a28820a1d084c506bebf3d950230caccd949
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-12-21 11:43:22 +0300

    Fix a type mismatch. the user_id should not be of UUID but instead of bson's ObjectId

--- src/domain/types/verification.rs
+++ src/domain/types/verification.rs
@@ -1,11 +1,12 @@
 use serde::{Serialize, Deserialize};
-use chrono::{DateTime, Utc};
 use sqlx::{types::Uuid, FromRow};
+use chrono::{DateTime, Utc};
+use super::Id;
 
 #[derive(Debug, Serialize, Deserialize, FromRow)]
 pub struct Verification {
     pub id: Uuid,
-    pub user_id: Uuid,
+    pub user_id: Id,
     pub code: String,
     pub created_at: DateTime<Utc>,
 }
